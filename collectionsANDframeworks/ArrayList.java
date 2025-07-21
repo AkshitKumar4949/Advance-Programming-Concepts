a57d574a1338be34bbd2faf1fb6d34666b0412c9
Akshit Kumar <akshitkumar4949@gmail.com>
2025-07-21 13:42:54 +0530

    commit 8

--- collectionsANDframeworks/ArrayList.java
+++ collectionsANDframeworks/ArrayList.java
@@ -1,5 +1,29 @@
 package collectionsANDframeworks;
 
-public class ArrayList {
-    
+import java.util.*;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(4);
+        list.add(69);
+        list.add(72);
+        list.add(1);
+        list.add(100);
+        System.out.println(list.get(2));
+        // Retrieves the element at the specified index.
+
+        System.out.println(list.get(4));
+        
+        list.remove(Integer.valueOf(100));
+        // Removes the first occurrence of the specified value.
+        // Use Integer.valueOf(x) to remove a specific number.
+
+        list.sort(null);
+        // Purpose: Sorts the list in natural (ascending) order.
+        // Passing null tells Java to use the default comparator (i.e., ascending order
+        // for numbers).
+
+        System.out.println(list);
+    }
 }
---
+++ collectionsANDframeworks/Linkedlist.java
@@ -0,0 +1,8 @@
+package collectionsANDframeworks;
+// Linnked List is a linear data structure where each element is a separate object called a node.
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        
+    }
+}
